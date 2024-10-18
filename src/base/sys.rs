19c67aa164e088a8ddf03e899f2df56516a2e029
c <iclings@qq.com>
2024-10-18 19:47:42 +0800

    process

--- src/base/sys.rs
+++ src/base/sys.rs
@@ -1,21 +1,10 @@
-use std::process::Command;
-
+#[cfg(target_os = "linux")]
 pub fn is_running(process: &str) -> bool {
-    #[cfg(target_os = "windows")]
-    {
-        let output = Command::new("tasklist")
-            .output()
-            .expect("failed to execute tasklist");
-        String::from_utf8_lossy(&output.stdout).contains(process)
-    }
+    use std::process::Command;
+    let output = Command::new("pgrep")
+        .arg(process)
+        .output()
+        .expect("failed to execute pgrep");
 
-    #[cfg(not(target_os = "windows"))]
-    {
-        let output = Command::new("pgrep")
-            .arg(process)
-            .output()
-            .expect("failed to execute pgrep");
-
-        !output.stdout.is_empty()
-    }
+    !output.stdout.is_empty()
 }
