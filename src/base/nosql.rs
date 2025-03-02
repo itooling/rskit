cb01470d102968c1ba2b9288ef5f42186dcf05e6
c <iclings@qq.com>
2025-03-02 14:54:36 +0800

    redis

--- src/base/nosql.rs
+++ src/base/nosql.rs
@@ -7,7 +7,7 @@ use redis::{Client, Commands, FromRedisValue, RedisError};
 pub static REDIS_CLIENT: LazyLock<Client> =
     LazyLock::new(|| Client::open("redis://127.0.0.1/").expect("connect redis error"));
 
-pub fn value<V: FromRedisValue>(key: &str) -> Result<V, RedisError> {
+pub fn get<V: FromRedisValue>(key: &str) -> Result<V, RedisError> {
     let mut conn = REDIS_CLIENT.clone();
     let v: V = conn.get(key)?;
     Ok(v)
